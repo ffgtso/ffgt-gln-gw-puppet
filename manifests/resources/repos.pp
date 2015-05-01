458e52127ab48c00b8b45f2c2902469fff8447e5
FFGT Admins <wusel+src@uu.org>
2015-05-01 22:34:56 +0000

    Alignment to current puppetlabs-apt

--- manifests/resources/repos.pp
+++ manifests/resources/repos.pp
@@ -5,23 +5,49 @@ class ff_gln_gw::resources::repos (
     location   => 'http://repo.universe-factory.net/debian/',
     release    => 'sid',
     repos      => 'main',
-    key        => '16EF3F64CB201D9C',
-    key_server => 'pgpkeys.mit.edu';
+    key        => {
+      'id'     => '16EF3F64CB201D9C',
+      'server' => 'pgpkeys.mit.edu',
+    },
+    include  => {
+      'src' => false,
+      'deb' => true,
+    }
   }
 
  apt::source { 'debian.draic.info':
     location    => 'http://debian.draic.info/',
     release     => 'wheezy',
     repos       => 'main',
-    include_src => false,
-    key_server  => 'pgpkeys.mit.edu';
+    include  => {
+      'src' => false,
+      'deb' => true,
+    }
   }
 
+  package { 'debian-keyring':
+    ensure => present
+  }
+  package { 'debian-archive-keyring':
+    ensure => present
+  }
   apt::source { 'debian-backports':
      location          => $debian_mirror,
-     required_packages => 'debian-keyring debian-archive-keyring',
      release           => 'wheezy-backports',
      repos             => 'main contrib',
-     include_src       => false,
+     include  => {
+      'src' => false,
+      'deb' => true,
+     }
+  }
+
+  apt::source { 'trusty-backports':
+    location => 'http://de.archive.ubuntu.com/ubuntu',
+    release => 'trusty-backports',
+    repos => 'main universe multiverse restricted',
+    key => {
+      id => '630239CC130E1A7FD81A27B140976EAF437D05B5',
+      server => 'pgp.mit.edu',
+    },
   }
 }
