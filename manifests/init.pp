f889b91d24a17c6509feed16c6027e9597e96ce5
Kai 'wusel' Siering <wusel+src@uu.org>
2015-08-22 01:43:42 +0200

    Create ff_gln_gw::gateway

--- manifests/init.pp
+++ manifests/init.pp
@@ -121,3 +121,81 @@ define ff_gln_gw::mesh(
   # ff_gln_gw::opkg::mirror
   # ff_gln_gw::firmware mirror
 }
+
+
+define ff_gln_gw::gateway(
+  $mesh_name,        # Name of your community, e.g.: Freifunk Gotham City
+  $mesh_code,        # Code of your community, e.g.: ffgc
+  $mesh_as,          # AS of your community
+  $mesh_mac,         # mac address mesh device: 52:54:00:bd:e6:d4
+  $range_ipv4,       # ipv4 range allocated to community in cidr notation, e.g. 10.35.0.1/16
+  $mesh_ipv4,        # ipv4 address in cidr notation, e.g. 10.35.0.1/19
+  $mesh_ipv6,        # ipv6 address in cidr notation, e.g. fd35:f308:a922::ff00/64
+  $mesh_peerings,    # path to the local peerings description yaml file
+  $have_mesh_peerings,
+)  {
+
+  # TODO We should handle parameters in a param class pattern.
+  # TODO Handle all git repos and other external sources in
+  #      a configuration class, so we can redefine sources.
+  # TODO Update README
+
+  include ff_gln_gw::ntp
+  include ff_gln_gw::maintenance
+  include ff_gln_gw::firewall
+
+  # Determine ipv{4,6} network prefixes and ivp4 netmask
+  $mesh_ipv4_prefix    = ip_prefix($mesh_ipv4)
+  $mesh_ipv4_prefixlen = ip_prefixlen($mesh_ipv4)
+  $mesh_ipv4_netmask   = ip_netmask($mesh_ipv4)
+  $mesh_ipv4_address   = ip_address($mesh_ipv4)
+
+  $mesh_ipv6_prefix    = ip_prefix($mesh_ipv6)
+  $mesh_ipv6_prefixlen = ip_prefixlen($mesh_ipv6)
+  $mesh_ipv6_address   = ip_address($mesh_ipv6)
+
+  Class['ff_gln_gw::ntp'] ->
+  ff_gln_gw::ntp::allow { "${mesh_code}":
+    ipv4_net => $mesh_ipv4,
+    ipv6_net => $mesh_ipv6
+  } ->
+  ff_gln_gw::named::listen { "${mesh_code}":
+    ipv4_address => $mesh_ipv4_address,
+    ipv6_address => $mesh_ipv6_address,
+  } ->
+  ff_gln_gw::named::allow {
+    "${mesh_code}_v4":
+      ip_prefix    => $mesh_ipv4_prefix,
+      ip_prefixlen => $mesh_ipv4_prefixlen;
+    "${mesh_code}_v6":
+      ip_prefix    => $mesh_ipv6_prefix,
+      ip_prefixlen => $mesh_ipv6_prefixlen;
+  }
+
+  if $ff_gln_gw::params::include_bird6 {
+    ff_gln_gw::bird6::mesh { "bird6-${mesh_code}":
+      mesh_code => $mesh_code,
+      mesh_ipv4_address => $mesh_ipv4_address,
+      mesh_ipv6_address => $mesh_ipv6_address,
+      mesh_peerings => $mesh_peerings,
+      site_ipv6_prefix => $mesh_ipv6_prefix,
+      site_ipv6_prefixlen => $mesh_ipv6_prefixlen,
+      icvpn_as => $mesh_as;
+    }
+  }
+  if $ff_gln_gw::params::include_bird4 {
+    ff_gln_gw::bird4::mesh { "bird4-${mesh_code}":
+      mesh_code => $mesh_code,
+      mesh_ipv4_address => $mesh_ipv4_address,
+      range_ipv4 => $range_ipv4,
+      mesh_ipv6_address => $mesh_ipv6_address,
+      mesh_peerings => $mesh_peerings,
+      site_ipv4_prefix => $mesh_ipv4_prefix,
+      site_ipv4_prefixlen => $mesh_ipv4_prefixlen,
+      icvpn_as => $mesh_as;
+    }
+  }
+
+  # ff_gln_gw::opkg::mirror
+  # ff_gln_gw::firmware mirror
+}
