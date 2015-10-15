0fab576a4c9753143d838e3ea9ae4e65d9adbe01
Kai 'wusel' Siering <wusel+src@uu.org>
2015-10-16 00:49:13 +0200

    Fixe name and argument.

--- manifests/gre.pp
+++ manifests/gre.pp
@@ -1,12 +1,12 @@
 define ff_gln_gw::gre::tunnel (
   $gre_yaml, # YAML data file for creating GRE tunnels
 ) {
-  file { "/tmp/prepare-gre-tunnels-${mesh_code}.sh":
+  file { "/tmp/prepare-gre-tunnels-${name}.sh":
     mode => "0755",
     content => template("ff_gln_gw/etc/network/prepare-gre-tunnels.erb")
   } ->
   exec { "prepare-gre-tunnels-${mesh_code}":
-    command => "/tmp/prepare-gre-tunnels-${mesh_code}.sh",
+    command => "/tmp/prepare-gre-tunnels-${name}.sh",
     cwd => "/tmp"
   }
 }
