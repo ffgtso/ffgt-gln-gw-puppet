079896e9f766bc6ef88a358ccd655f47458af964
wusel42 <wusel+src@uu.org>
2016-02-29 22:43:54 +0100

    Cron

--- manifests/monitor/rrd_traffic.pp
+++ manifests/monitor/rrd_traffic.pp
@@ -21,4 +21,22 @@ define ff_gln_gw::monitor::rrd_traffic (
       group => 'root',
       content => template('ff_gln_gw/root/rrd_traffic_upload.sh.erb');
   }
+
+  cron {
+   'rrd_traffic_slave':
+     command => '/root/rrd_traffic_slave.pl --slave >/dev/null 2>&1'.
+     user    => root,
+     minute  => '*/5',
+     hour    => '*',
+     require => File['/root/rrd_traffic_slave.pl'];
+  }
+
+  cron {
+   'rrd_traffic_upload':
+     command => 'sleep 20 ; /root/rrd_traffic_upload.sh >/dev/null 2>&1'.
+     user    => root,
+     minute  => '*/5',
+     hour    => '*',
+     require => File['/root/rrd_traffic_upload.sh'];
+  }
 }
