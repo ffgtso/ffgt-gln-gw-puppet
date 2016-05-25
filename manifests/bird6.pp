eb27b73507d79352030bffb4d1e7fd9a462a0326
Kai 'wusel' Siering <wusel+src@uu.org>
2016-05-25 04:06:08 +0200

    Fixes

--- manifests/bird6.pp
+++ manifests/bird6.pp
@@ -118,6 +118,7 @@ define ff_gln_gw::bird6::ospf (
   $ospf_links,    # YAML data file for local interconnects
   $have_ospf_peerings = "no", # Actually require & use $ospf_peerings
   $have_ospf_links = "no"     # Actually require & use $ospf_links
+  $ospf_type = "root",        # root/leaf: root reexports routes, leaf only exports statics.
 ) {
 
   include ff_gln_gw::bird6
