ba605c56820554ab2e7a904232fa7efcde0741da
wusel42 <wusel+src@uu.org>
2016-09-24 01:01:02 +0200

    Need mesh_code

--- manifests/uplink.pp
+++ manifests/uplink.pp
@@ -349,6 +349,7 @@ define ff_gln_gw::uplink::tunnelDS (
   $tunnel_mtu = 1426,
   $v6_network,
   $remote_as,
+  $mesh_code,
 ) {
   include ff_gln_gw::resources::network
   include ff_gln_gw::resources::sysctl
@@ -434,6 +435,7 @@ define ff_gln_gw::uplink::nattunnel (
   $nat_network,
   $tunnel_network = "127.0.0.0/8",
   $bgp_local_pref = 100
+  $mesh_code,
 ) {
   include ff_gln_gw::resources::network
   include ff_gln_gw::resources::sysctl
@@ -525,6 +527,7 @@ define ff_gln_gw::uplink::nattunnelDS (
   $v6_network,
   $tunnel_network = "127.0.0.0/8",
   $bgp_local_pref = 100,
+  $mesh_code,
 ) {
   include ff_gln_gw::resources::network
   include ff_gln_gw::resources::sysctl
