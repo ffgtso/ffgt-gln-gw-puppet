662b5c34b2dc5d74e177deb6f65ee6cc9e4f03b6
wusel42 <wusel+src@uu.org>
2016-10-05 13:25:13 +0200

    Rework tun-ip.sh, make IP addresses configurable.

---
+++ manifests/auto_gen_tunnel.pp
@@ -0,0 +1,14 @@
+class ff_gln_gw::auto_gen_tunnel (
+  $ipv4base, # "10.234",
+  $ipv6base  # "2a03:2260:117:0"
+) {
+  file {
+    '/usr/locab/bin/tun-ip.sh':
+      ensure => file,
+      mode => "0755",
+      owner => root,
+      group => root,
+      content => template("ff_gln_gw/usr/local/bin/tun-ip.sh.erb");
+  }
+}
+
