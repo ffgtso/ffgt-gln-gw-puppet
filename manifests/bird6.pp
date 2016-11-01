aad2bd75b81808d47f566023b66bf4f57b554667
wusel42 <wusel+src@uu.org>
2016-11-01 01:55:40 +0100

    Fixes

--- manifests/bird6.pp
+++ manifests/bird6.pp
@@ -351,7 +351,7 @@ define ff_gln_gw::bird6::ebgp (
       "bird6-ebgp-special-${name}":
         path => '/etc/bird/bird6.conf.inc',
         line => "include \"/etc/bird/bird6.conf.d/03-ebgp-A-${name}-init.conf\";",
-        replace => 'no',
+        replace => 'false',
         require => File['/etc/bird/bird6.conf.inc'],
         notify  => Service['bird6'];
     }
