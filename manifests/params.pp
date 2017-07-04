c7f5f4e6babd9476a692e6ea0a42c4bf0390a945
wusel42 <wusel+src@uu.org>
2017-07-04 13:18:45 +0200

    Make router_id default v4 loopback addr.

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,7 +3,7 @@ class ff_gln_gw::params (
   $icvpn_as,  # Main AS number of this host, e.g. 65035
               # This number will be used for the main bird configuration
   $wan_devices, # Network devices which are in the wan zone
-  $debian_mirror = 'http://ftp.de.debian.org/debian/',
+  $debian_mirror = 'https://ftp.de.debian.org/debian/',
   $include_bird4 = true, # support bird
   $include_bird6 = true, # support bird6
   $include_chaos_routes = "no",
@@ -11,7 +11,7 @@ class ff_gln_gw::params (
   $provides_uplink = "no",
   $ipv6_main_prefix = "",
   $loopback_ipv6 = "::1/128",
-  $loopback_ipv4 = "127.0.0.1",
+  $loopback_ipv4 = $router_id,
   # Default values for ff_gln_gw config
   $maintenance = 0, # Shall the maintenance mode be active after installation
 ) {
