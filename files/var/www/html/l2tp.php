7d1f2706a0d2341f9e5c1797e4eb6cbc15fcbb7a
wusel42 <wusel+src@uu.org>
2017-11-25 18:35:31 +0100

    Add primitive locking

--- files/var/www/html/l2tp.php
+++ files/var/www/html/l2tp.php
@@ -4,11 +4,18 @@
    $mac=htmlspecialchars(strtolower($_GET["primarymac"]));
    $port=htmlspecialchars(strtolower($_GET["port"]));
    $sessionid=sprintf("%d", hexdec(substr($mac, 8)));
+   $lockfile=sprintf("/tmp/l2tp-%s.lock", $mac);
+
+   $lockfh = fopen($lockfile, "x");
+   if(!$lockfh) {
+     printf("LCK");
+     exit(0);
+   }
 
    if(strlen($publicip) > 1 && strlen($mac) == 12 && $port > 9999) {
       $line=sprintf("/usr/local/bin/refresh-l2tp.sh %s %s %s", $mac, $publicip, $port);
       $retline=system($line, $rc);
-      /*printf("%s %s %s %s\n", $filenameup, $sessionid, $publicip, $mac);*/
-      /*printf("%s %s 10000\n", $sessionid, $publicip);*/
+      fclose($lockfh);
+      unlink($lockfile);
    }
 ?>
