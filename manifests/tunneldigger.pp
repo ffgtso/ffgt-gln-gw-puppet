8de862fbfc069ebee06414aa6bc4d045b64b1c9b
wusel42 <wusel+src@uu.org>
2019-02-17 00:30:13 +0100

    Incorporate & adapt tunneldigger stuff from Freifunk Berlin

---
+++ manifests/tunneldigger.pp
@@ -0,0 +1,146 @@
+class tunneldigger(
+  $install_dir='/srv/tunneldigger/tunneldigger',
+  $revision='master',
+  $virtualenv="/srv/tunneldigger/env_tunneldigger",
+  $address,
+  $port='53,123,8942',
+  $interface,
+  $max_tunnels='1024',
+  $port_base='10001',
+  $tunnel_id_base='100',
+  $namespace='mesh',
+  $connection_rate_limit='10',
+  $pmtu='0',
+  $verbosity='DEBUG',
+  $log_ip_addresses='true',
+  $templates_dir='tunneldigger',
+  $functions='bridge_functions.sh',
+  $session_up='setup_interface.sh',
+  $session_pre_down='teardown_interface.sh',
+  $session_down='',
+  $session_mtu_changed='',
+  $bridge_mac,
+  $bridge_mtu='1364',
+  $systemd='1'
+) {
+
+  class { 'python':
+    version    => 'system',
+    pip        => 'present',
+    dev        => 'present',
+    virtualenv => 'present',
+    gunicorn   => 'absent',
+  }
+
+  package { [
+    'iproute',
+    'bridge-utils',
+    'libnetfilter-conntrack-dev',
+    'libnfnetlink-dev',
+    'libffi-dev',
+    'libevent-dev',
+    'ebtables'
+  ]:
+    ensure => present,
+  }
+
+  vcsrepo { $install_dir:
+    ensure   => present,
+    provider => git,
+    source   => 'https://github.com/wlanslovenija/tunneldigger.git',
+    revision => $revision,
+    require  => [
+      Package['git']
+    ]
+  }
+
+  python::virtualenv { $virtualenv:
+    ensure       => present,
+    notify       => Exec['setup']
+  }
+
+  exec { 'setup':
+    command => "${virtualenv}/bin/python setup.py install",
+    path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+    cwd => "${install_dir}/broker",
+  }
+
+
+  file { "${install_dir}/broker/l2tp_broker.cfg":
+    ensure    => file,
+    content   => template('tunneldigger/l2tp_broker.cfg.erb'),
+    require   => Exec['setup'],
+  }
+
+  $scripts = "${install_dir}/broker/scripts"
+
+  if $functions {
+    file { "${scripts}/${functions}":
+      ensure    => file,
+      content   => template("${templates_dir}/${functions}.erb"),
+      require   => Exec['setup'],
+    }
+  }
+
+  if $session_up {
+    file { "${scripts}/${session_up}":
+      ensure    => file,
+      content   => template("${templates_dir}/${session_up}.erb"),
+      require   => Exec['setup'],
+    }
+  }
+
+  if $session_pre_down {
+    file { "${scripts}/${session_pre_down}":
+      ensure    => file,
+      content   => template("${templates_dir}/${session_pre_down}.erb"),
+      mode      => '744',
+      require   => Exec['setup'],
+    }
+  }
+
+  if $session_down {
+    file { "${scripts}/${session_down}":
+      ensure    => file,
+      content   => template("${templates_dir}/${session_down}.erb"),
+      mode      => '744',
+      require   => Exec['setup'],
+    }
+  }
+
+  if $session_mtu_changed {
+    file { "${scripts}/${session_mtu_changed}":
+      ensure    => file,
+      content   => template("${templates_dir}/${session_mtu_changed}.erb"),
+      mode      => '744',
+      require   => Exec['setup'],
+    }
+  }
+
+  file { "${install_dir}/broker/scripts/tunneldigger-broker":
+    ensure    => file,
+    content   => template('tunneldigger/tunneldigger-broker.erb'),
+    require   => Exec['setup'],
+  }
+
+  if $systemd == '1' {
+    file { '/etc/systemd/system/tunneldigger.service':
+      ensure    => file,
+      content   => template('tunneldigger/tunneldigger.service.erb'),
+      require   => Exec['setup'],
+      notify    => Service['tunneldigger'],
+    }
+  }
+
+  service { "tunneldigger":
+    ensure      => 'running',
+    enable      => 'true',
+  }
+
+  file { '/etc/modules-load.d/tunneldigger.conf':
+    ensure      => file,
+    content     => template('tunneldigger/modules.conf.erb'),
+    require     => Exec['setup'],
+  }
+
+}
