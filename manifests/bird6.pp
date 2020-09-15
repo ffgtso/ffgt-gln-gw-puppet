7a9e71e7d5796ce5842b1c36f0820e8bb1451efe
Kai Siering <k.siering@team.mail.de>
2020-09-16 00:31:21 +0200

    Undo

--- manifests/bird6.pp
+++ manifests/bird6.pp
@@ -331,7 +331,7 @@ define ff_gln_gw::bird6::ibgp (
   $our_as = $ff_gln_gw::params::icvpn_as,
   $next_hop_self = "",
   $bgp_options = "",
-  $no_export_prefix = "none",
+  $dont_export_prefix = "none",
   $dfz = "",
   $mesh_code = $ff_gln_gw::params::mesh_code,
 ) {
